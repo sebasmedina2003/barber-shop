4c87e8a86690a1d95d1c2b1f6244f8c2df30dfe1
Sebastian Medina <sebasmedina2003@gmail.com>
2024-07-21 17:14:41 -0400

    add: register cliente

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\ClientModel;
 use Auth;
 use Db;
 
 class AuthController
 {
-    public function login(Request $request)
-    {
+    public function login(Request $request){
         $validatedData = $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string|min:6',
@@ -36,6 +37,37 @@ class AuthController
 
         // Return a response
         return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+
+    public function store_user(Request $request){
+        $validatedData = $request->validate([
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6',
+            'name' => 'required|string',
+            'last_name' => 'required|string',
+
+        ]);
+
+        try{
+            Db::transaction(function() use($validatedData){
+                $user = new User();
+                $user->email = $validatedData['email'];
+                $user->password = bcrypt($validatedData['password']);
+                $user->is_barber = false;
+                $user->save();
+    
+                $client = new ClientModel();
+                $client->user_id = $user->id;
+                $client->name = $validatedData['name'];
+                $client->last_name = $validatedData['last_name'];
+                $client->save();
+            });
+
+        } catch (UniqueConstraintViolationException $e){
+            return response()->json(['message' => 'Email already exists'], 400);
+        }
+
+        return response()->json(['message' => 'User created successfully'], 201);
     }
 
 }
